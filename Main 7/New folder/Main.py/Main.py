79560cfb920a1b09e0d195ee70657be043f572e4
Nidhi1228 <ranjitha.sekhar@gmail.com>
2025-09-07 10:43:10 +0400

    V2

---
+++ Main 7/New folder/Main.py/Main.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://official-joke-api.appspot.com/random_joke"
+
+response = requests.get(url)
+
+
+if response.status_code == 200:
+
+        joke = response.json()
+
+        print(f"{joke['setup']} - {joke['punchline']}")
+
+else:
+
+      print("Failed to")
